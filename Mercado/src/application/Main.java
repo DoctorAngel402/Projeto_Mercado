d2fbaf5378247f3893dfbf99704c6d981add0553
sistema.noturno <sistema.noturno@LAB-03-03.sistemafieto.rede>
2025-07-30 20:42:52 -0300

    criacao das classes view e controller, e configuração do SceneBiulder
    Além de alterar na class main o public void Start

--- Mercado/src/application/Main.java
+++ Mercado/src/application/Main.java
@@ -6,20 +6,28 @@ import java.util.ArrayList;
 import connectionFactory.ConnectionDatabase;
 import dao.ClienteDAO;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import model.Cliente;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
+	
+	private static Stage stage;
+	private static Scene main;
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			
+			stage = primaryStage;
+			
+			Parent fxmlLogin = FXMLLoader.load(getClass().getResource("/view/viewLogin.fxml"));
+			main = new Scene(fxmlLogin);
+			
+			primaryStage.setScene(main);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
